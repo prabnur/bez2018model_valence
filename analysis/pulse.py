a1ef0423fb7831e05d4b57d813b9fa66c782dc88
prabnur <prabnurbal@gmail.com>
2023-09-27 19:02:01 -0400

    Save point

--- analysis/pulse.py
+++ analysis/pulse.py
@@ -6,15 +6,53 @@ import matplotlib.pyplot as plt
 TAU = 1e-3  # 0.001 ms
 MAX = 0.25
 
-def generate_pulse_vector(spikes):
+def generate_pulse_vector(spikes, tau=TAU, duration=MAX):
     """Generates a pulse vector from a spike 3D array."""
-    round_factor = len("{:.10f}".format(TAU).split('.')[1])
+    round_factor = len("{:.10f}".format(tau).split('.')[1])
     rounded_spikes = np.round(spikes, round_factor)
-    pulse = np.zeros(int(MAX / TAU))
-    for cf in rounded_spikes:
-        for anf in cf:
-            for spike in anf:
-                if spike != 0:
-                    pulse[int(spike / TAU)] += 1
+    
+    pulse = np.zeros(int(duration / tau))
+    
+    non_zero_indices = np.argwhere(rounded_spikes != 0)
+    
+    for i, j, k in non_zero_indices:
+        index = int(rounded_spikes[i, j, k] / tau)
+        pulse[index] += 1
+
     return pulse
 
+def generate_pulse_vector_opt(spikes, tau=TAU, duration=MAX):
+    """
+    Generates a pulse vector from a spike 3D array.
+    Not as accurate but significantly faster.
+    """
+    round_factor = len("{:.10f}".format(tau).split('.')[1])
+    rounded_spikes = np.round(spikes, round_factor)
+
+    pulse = np.zeros(int(duration / tau))
+
+    non_zero_spikes = rounded_spikes[rounded_spikes != 0]
+    indices = (non_zero_spikes / tau).astype(int)
+    
+    np.add.at(pulse, indices, 1)
+    
+    return pulse
+
+def euclidean_distance(u, v):
+    """Compute the Euclidean distance between two vectors."""
+    return np.linalg.norm(u - v)
+
+def prediction_error(given, expected):
+    """Compute the prediction error between two vectors."""
+    return np.sum(given - expected)
+
+def cosine_similarity(u, v):
+    """Compute the cosine similarity between two vectors."""
+    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+def analyze(spikes_list, comparison_function):
+    pulses = [generate_pulse_vector(spikes) for spikes in spikes_list]
+    comps = [comparison_function(pulse, pulses[0]) for pulse in pulses]
+    for comp in comps:
+        print(comp)
+    plt.plot(comps)
